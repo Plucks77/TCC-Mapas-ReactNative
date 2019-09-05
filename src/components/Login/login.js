4d073cac582a0cf364d8e2f3d481ca85482bec17
Pedro <pedro_leoti@hotmail.com>
2019-09-05 11:08:16 -0300

    Adição do AsyncStorage

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,22 +1,35 @@
 import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 import { Container, Inputs, Input, Botao } from "./style";
+import AsyncStorage from "@react-native-community/async-storage";
 
 // import { Container } from './styles';
 
 export default function Login({ navigation }) {
   const [user, setUser] = useState("");
 
+  useEffect(() => {
+    AsyncStorage.getItem("email").then(email => {
+      if (email) {
+        //navigation.navigate("Mapa");
+      }
+    });
+  }, []);
+
   async function handleLogin() {
+    await AsyncStorage.setItem("email", user);
     navigation.navigate("Mapa");
   }
   return (
     <Container>
       <Inputs>
         <Input
-          placeholder="Digite seu login"
+          placeholder="Digite seu email"
           autoCapitalize="none"
           autoCorrect={false}
+          onChangeText={setUser}
+          value={user}
+          keyboardType="email-address"
         ></Input>
 
         <Input
