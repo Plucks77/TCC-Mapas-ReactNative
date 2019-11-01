a48acc0e524d1c7070da4968c58a598130d1533c
Pedro Lucas <pedro_leoti@hotmail.com>
2019-11-01 10:56:45 -0300

    Melhorias na tela de cadastro de usuário, como adição de máscaras para data de nascimento e cpf

--- src/components/CadastroUsuario/cadastro.js
+++ src/components/CadastroUsuario/cadastro.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import api from "../../services/api";
-import { Text, View, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import AsyncStorage from "@react-native-community/async-storage";
 import Icon from "react-native-vector-icons/FontAwesome";
+import { TextInputMask } from "react-native-masked-text";
+import Lottie from "lottie-react-native";
+import loading from "../../../assets/loading.json";
 import {
   Container,
   TituloArea,
@@ -14,29 +17,45 @@ import {
 } from "./style";
 
 export default function CadastroUsuario(props) {
-  const [nome, setNome] = useState("");
-  const [email, setEmail] = useState("");
-  const [senha, setSenha] = useState("");
-  const [data_nascimento, setData_nascimento] = useState("");
-  const [cpf, setCpf] = useState("");
-  const [nick, setNick] = useState("");
+  const [user, setUser] = useState({
+    nome: "",
+    email: "",
+    senha: "",
+    data_nascimento: "",
+    cpf: "",
+    nick: ""
+  });
+  const [ready, setReady] = useState(true);
 
   async function handleCadastro() {
-    try {
-      const response = await api.post("/usuario/cadastrar", {
-        nome,
-        email,
-        senha,
-        data_nascimento,
-        cpf,
-        nick
-      });
-      // console.log(response.data);
-      await AsyncStorage.setItem("user_id", response.data.user_id.toString());
-      await AsyncStorage.setItem("user_token", response.data.token);
-      props.navigation.navigate("Mapa");
-    } catch (erro) {
-      console.log("O seguinte erro ocorreu: " + erro);
+    if (
+      user.nome != "" &&
+      user.email != "" &&
+      user.senha != "" &&
+      user.data_nascimento != "" &&
+      user.cpf != "" &&
+      user.nick != ""
+    ) {
+      try {
+        setReady(false);
+        const response = await api.post("/usuario/cadastrar", {
+          nome: user.nome,
+          email: user.email,
+          senha: user.senha,
+          data_nascimento: user.data_nascimento,
+          cpf: user.cpf,
+          nick: user.nick
+        });
+        // console.log(response.data);
+        await AsyncStorage.setItem("user_id", response.data.user_id.toString());
+        await AsyncStorage.setItem("user_token", response.data.token);
+        props.navigation.navigate("Mapa");
+      } catch (erro) {
+        setReady(true);
+        console.log("O seguinte erro ocorreu: " + erro);
+      }
+    } else {
+      alert("Preencha todos os campos!");
     }
   }
 
@@ -44,7 +63,7 @@ export default function CadastroUsuario(props) {
     props.navigation.navigate("Login");
   }
 
-  return (
+  return ready ? (
     <Container>
       <TouchableOpacity
         style={{
@@ -60,16 +79,69 @@ export default function CadastroUsuario(props) {
         <Titulo> Cadastrar </Titulo>
       </TituloArea>
       <InputsArea>
-        <Input onChangeText={setNome} value={nome} placeholder="Nome" />
-        <Input placeholder="Email" onChangeText={setEmail} value={email} />
-        <Input placeholder="Senha" onChangeText={setSenha} value={senha} />
         <Input
-          placeholder="Data de nascimento"
-          onChangeText={setData_nascimento}
-          value={data_nascimento}
+          onChangeText={e => setUser({ ...user, nome: e })}
+          value={user.nome}
+          placeholder="Nome"
         />
-        <Input placeholder="CPF" onChangeText={setCpf} value={cpf} />
-        <Input placeholder="Nick" onChangeText={setNick} value={nick} />
+
+        <Input
+          placeholder="Email"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          onChangeText={e => setUser({ ...user, email: e })}
+          value={user.email}
+        />
+        <Input
+          placeholder="Senha"
+          secureTextEntry={true}
+          autoCapitalize="none"
+          autoCorrect={false}
+          onChangeText={e => setUser({ ...user, senha: e })}
+          value={user.senha}
+        />
+        <TextInputMask
+          style={{
+            width: 300,
+            height: 60,
+            fontSize: 15,
+            color: "#df4723",
+            marginBottom: 1,
+            marginHorizontal: 10,
+            borderBottomWidth: 1
+          }}
+          placeholder="Data de nascimento"
+          placeholderTextColor="#df4723"
+          type={"datetime"}
+          options={{
+            format: "DD/MM/YYYY"
+          }}
+          value={user.data_nascimento}
+          onChangeText={e => setUser({ ...user, data_nascimento: e })}
+        />
+
+        <TextInputMask
+          style={{
+            width: 300,
+            height: 60,
+            fontSize: 15,
+            color: "#df4723",
+            marginBottom: 1,
+            marginHorizontal: 10,
+            borderBottomWidth: 1
+          }}
+          placeholder="Cpf"
+          placeholderTextColor="#df4723"
+          type={"cpf"}
+          value={user.cpf}
+          onChangeText={e => setUser({ ...user, cpf: e })}
+        />
+
+        <Input
+          placeholder="Nick"
+          onChangeText={e => setUser({ ...user, nick: e })}
+          value={user.nick}
+        />
       </InputsArea>
 
       <BotaoArea>
@@ -78,5 +150,16 @@ export default function CadastroUsuario(props) {
         </Botao>
       </BotaoArea>
     </Container>
+  ) : (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#121212"
+      }}
+    >
+      <Lottie source={loading} autoSize resizeMode="contain" autoPlay loop />
+    </View>
   );
 }
